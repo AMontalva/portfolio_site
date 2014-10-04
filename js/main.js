791a47d0160acc6b37ae8fc89b3ca16637fbe24b
AMontalva <montalva@uci.edu>
2014-10-03 19:38:15 -0700

    big update

--- js/main.js
+++ js/main.js
@@ -1,1 +1,41 @@
+    // $(function(){
+    //     var w, h, p1;
 
+    //     $(".hoverImg").hover(function() {
+    //         w = $(".hoverImg").width();
+    //         h = $(".hoverImg").height();
+
+    //         p1 = $("<div id='x'></div>");
+    //         p1.css({"width": w, "height": h, "background": "rgba(0, 0, 0, 0.70)", "position": "absolute", "margin-top": -h});
+    //         $(p1).hide().appendTo(this).fadeIn(500);        
+    //     }, function() {
+    //         $(p1).fadeOut(500);
+    //     });
+
+
+
+
+
+
+
+    // $(function(){
+    //     var w, h, p1;
+
+    //     $(".hoverImg").hover(function() {
+    //         w = $(".hoverImg").width();
+    //         h = $(".hoverImg").height();
+
+    //         p1 = $("<div id='hoverId'></div>");
+    //         p1.css({"width": w, "height": h, "background": "rgba(0, 0, 0, 0.70)", "position": "absolute", "margin-top": -h});
+            
+    //         p1Button = $("<a href='#'>Project 1</a>");
+    //         p1Button.css({"width": "100px", "height": "20px", "position": "absolute", "z-index": "99", "background-color": "#FC9D4B", "color": "#FFFFFF"});
+
+    //         p1Button.appendTo(this);
+
+    //         p1.hide().appendTo(this).fadeIn(500);  
+    //     }, function() {
+    //         p1.fadeOut(500);
+    //     });
+
+    // });
